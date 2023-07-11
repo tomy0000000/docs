4af64b1bfb3c4ff00df9ef915b776c6ceec11272
Tomy Hsieh <git@tomy.me>
2023-07-11 13:25:16 +0800

    🗄 chore: SortableTable component

---
+++ src/components/SortableTable/index.tsx
@@ -0,0 +1,69 @@
+// ----------------------------------------------------------------------------
+// Please note that this component is deliberately NOT using a CCS stylesheet
+// because react-table is ui-agnostic and the table will therefore inherit
+// Docusaurus table styling. If additional styling ever becomes required please
+// consider using either "css modules" or "styled components".
+//
+// Code sandbox at https://codesandbox.io/s/pester-data-table-basic-1x0mw
+// ----------------------------------------------------------------------------
+import React from "react";
+import {
+  flexRender,
+  getCoreRowModel,
+  getSortedRowModel,
+  SortingState,
+  useReactTable,
+} from "@tanstack/react-table";
+import "./style.css";
+
+export default function SortableTable({ columns, data }): JSX.Element {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const table = useReactTable({
+    data,
+    columns,
+    state: {
+      sorting,
+    },
+    onSortingChange: setSorting,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+  });
+
+  return (
+    <table>
+      <thead>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map((header) => (
+              <th
+                key={header.id}
+                colSpan={header.colSpan}
+                onClick={header.column.getToggleSortingHandler()}
+              >
+                {flexRender(
+                  header.column.columnDef.header,
+                  header.getContext()
+                )}
+                {{
+                  asc: " 🔼",
+                  desc: " 🔽",
+                }[header.column.getIsSorted() as string] ?? null}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody>
+        {table.getRowModel().rows.map((row) => (
+          <tr key={row.id} id={row.id}>
+            {row.getVisibleCells().map((cell) => (
+              <td key={cell.id}>
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
